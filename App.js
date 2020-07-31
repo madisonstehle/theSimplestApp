afd7586d1a751b7eb1971108db7f7e0ea2b5182c
Madison Stehle <mnstehle3@gmail.com>
2020-07-31 16:32:54 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+  let quotes = [
+    {
+      author: 'William James',
+      text: 'Act as if what you do makes a difference. IT DOES.'
+    },
+    {
+      author: 'Winston Churchill',
+      text: 'Success is not final, failure is not fatal: it is the COURAGE TO CONTINUE that counts.'
+    },
+    {
+      author: 'Helen Keller',
+      text: 'Never bend your head. Always hold it high. Look the world straight in the eye.'
+    }
+  ];
+
+  const randomizer = (arr) => {
+    return Math.floor(Math.random()*3);
+  }
+
+  const createTwoButtonAlert = () => { 
+    let idx = randomizer(quotes);
+    
+    Alert.alert(
+      `${quotes[idx].author} says:`,
+      quotes[idx].text,
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "Oh Thanks!", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Are you feeling blue? Are things just hard? Do you have a test looming and need to be reminded that everything is going to be ok? Or are you just looking for some words of encouragement while the world’s on fire? 
+      </Text>
+      <Button title={"Look no further!!!"} onPress={createTwoButtonAlert} />
       <StatusBar style="auto" />
     </View>
   );
